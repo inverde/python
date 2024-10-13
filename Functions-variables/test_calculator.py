fb0975a41b0f273c7689ef34d8931bce81ae33d7
inverde <mdelossantos@yahoo.com>
2024-10-13 16:58:46 +0000

    Sun, Oct 13, 2024, 12:58 PM -04:00

---
+++ Functions-variables/test_calculator.py
@@ -0,0 +1,12 @@
+# Show unintended concatenation of strings
+
+# Ask the user for x and y values
+x = input("Por favor, digite el valor de x:")
+y = input("Por favor, digite el valor de y:")
+
+# Sum x + y
+z = x + y
+print(z)
+
+def test_sum():
+    assert z == 9
