226d2296b6097281b35b39625b9f35e6084a30e5
inverde <mdelossantos@yahoo.com>
2024-10-15 18:16:48 +0000

    Tue, Oct 15, 2024, 2:16 PM -04:00

---
+++ Conditionals/logical_operators.py
@@ -0,0 +1,13 @@
+# logical operators
+"""
+Logical operators are used to connect more than boolean expressions onto a conditional statement.
+
+Logical operators:
+1. and (Conditional statement is true if and only if both boolean expressions are true)
+2. or (Conditional statement is true if either one is true or both are true)
+3. not (Conditional statement is true if boolean expression is false)
+
+In Python logical operators operates not only over boolean expressions but also over values.
+When in doubt please only used operators with booleans
+
+"""
