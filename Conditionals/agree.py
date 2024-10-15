ad230859910a6e2d49cd34001712df0c2aa4e52b
inverde <mdelossantos@yahoo.com>
2024-10-15 04:12:44 +0000

    Tue, Oct 15, 2024, 12:12 AM -04:00

--- Conditionals/agree.py
+++ Conditionals/agree.py
@@ -59,14 +59,14 @@ def std_supporting_ballot():
 set_std_ballot()
 
 if is_std_ballot():
-    SUPPORTING_VOTE(c_STD) = std_supporting_ballot()
+    SUPPORTING_VOTE[c_STD] = std_supporting_ballot()
 else:
-    SUPPORTING_VOTE(c_NOR) = supporting_ballot()
+    SUPPORTING_VOTE[c_NOR] = supporting_ballot()
 
 # Voting pro and against with either of the ballots
-if (is_std_ballot() and SUPPORTING_VOTE(c_STD)) or (not is_std_ballot() and SUPPORTING_VOTE(c_NOR)):
+if (is_std_ballot() and SUPPORTING_VOTE[c_STD]) or (not is_std_ballot() and SUPPORTING_VOTE[c_NOR]):
     print("Plus Vote")
-elif (is_std_ballot and not SUPPORTING_VOTE(c_STD)) or (not is_std_ballot and not SUPPORTING_VODE(c_NOR)):
+elif (is_std_ballot and not SUPPORTING_VOTE[c_STD]) or (not is_std_ballot and not SUPPORTING_VODE[c_NOR]):
     print("Minus Vote")
 
 
