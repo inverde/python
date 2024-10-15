594544b3286bc790aec139d57b5fd354a2ccdeda
inverde <mdelossantos@yahoo.com>
2024-10-15 02:02:21 +0000

    Mon, Oct 14, 2024, 10:02 PM -04:00

--- Conditionals/agree.py
+++ Conditionals/agree.py
@@ -1,3 +1,10 @@
 # Make function module for conditional samples
 c_YES = 'yes'
+c_NO = 'no'
 
+# Define vote
+def supporting_ballot(on_support = c_NO):
+    if on_support == c_YES:
+        return True
+    else:
+        return False
