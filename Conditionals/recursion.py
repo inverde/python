a30409c3f828a10aabd36afc2a4960183fdc8a0c
inverde <mdelossantos@yahoo.com>
2024-10-15 18:31:27 +0000

    Tue, Oct 15, 2024, 2:31 PM -04:00

--- Conditionals/recursion.py
+++ Conditionals/recursion.py
@@ -3,12 +3,14 @@
 Recursions are used instead of loops to iterate over multiple instructions several times
 Recursions are usually more inefficient and slower than regular loops but logically
 make our program very easy to understand and save the programmers numerous instructions.
-""""
-
+"""
+# Defines a recursive function to count n times
 def countdown(n):
     count = n
     if count <= 0:
         return 0
     else:
+        print(count)
         return countdown(n-1)
 
+countdown(5)
