156827aa4a29901bf99657aca06275509e8b43ed
inverde <mdelossantos@yahoo.com>
2024-10-15 17:19:03 +0000

    Tue, Oct 15, 2024, 1:19 PM -04:00

---
+++ Conditionals/comparison_operators.py
@@ -0,0 +1,10 @@
+# Comparison operator are used in boolean expressions to determine its value
+
+# Equal comparison
+income = 15000
+
+# boolean expression
+low_income = income > 0 and income < 50000
+
+print(type(low_income))
+
