cbd9fb26787d551365a4dee680e7e676e4ef1a08
inverde <mdelossantos@yahoo.com>
2024-10-15 03:08:54 +0000

    Mon, Oct 14, 2024, 11:08 PM -04:00

--- Conditionals/agree.py
+++ Conditionals/agree.py
@@ -18,8 +18,8 @@ def set_std_ballot(toggle= c_TRUE):
         STD_BALLOT[c_ON] = toggle
         STD_BALLOT[c_OFF] = not toggle
     else:
-        STD_BALLOT[c_OFF] = not toggle
-        STD_BALLOT[c_ON] =  toggle
+        STD_BALLOT[c_OFF] = toggle
+        STD_BALLOT[c_ON] =  not toggle
 
 
 # Get vote from citizen
