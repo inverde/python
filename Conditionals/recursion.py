bbdde90d2bdf533b81e608986236b58f853dc01f
inverde <mdelossantos@yahoo.com>
2024-10-15 18:25:26 +0000

    Tue, Oct 15, 2024, 2:25 PM -04:00

---
+++ Conditionals/recursion.py
@@ -0,0 +1,4 @@
+# In programming recursion is called when a program call itself
+"""
+Recursions are used instead of loops to iterate over multiple instructions several times
+Recursions are usually more inefficient and slower than regular loops but logically
