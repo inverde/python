1f41017f53aa9f7a74ce4981061dc260bee4645f
inverde <mdelossantos@yahoo.com>
2024-10-15 17:28:09 +0000

    Tue, Oct 15, 2024, 1:28 PM -04:00

--- Conditionals/comparison_operators.py
+++ Conditionals/comparison_operators.py
@@ -3,10 +3,15 @@
 # Equal comparison
 income = 15000
 
-# 
+# Conditional statement
 low_income = income > 0 and income < 50000
 
 print(type(low_income))
 
 print(low_income)
 
+# Conditional Execution (if clause)
+
+# Conditionally print the economic class
+if low_income:
+    print('Low income class')
