3a7340dc7ee1c0df18f3f14c3012ecef729cb351
inverde <mdelossantos@yahoo.com>
2024-10-15 02:20:52 +0000

    Mon, Oct 14, 2024, 10:20 PM -04:00

--- Conditionals/agree.py
+++ Conditionals/agree.py
@@ -15,7 +15,7 @@ def supporting_ballot():
 def std_supporting_ballot():
     global c_Yes
     vote = input("Do you agree with this reform?: ")
-    if vote.strip().lower() == c_YES or vote.strip().lower() == :
+    if vote.strip().lower() == c_YES or vote.strip().lower().startwith == 'y':
         return True
     else:
         return False
