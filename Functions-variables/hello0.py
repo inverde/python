9bbda77b37f2dde0b093fec67290eee2659302bf
inverde <mdelossantos@yahoo.com>
2024-10-16 04:30:05 +0000

    Wed, Oct 16, 2024, 12:30 AM -04:00

--- Functions-variables/hello0.py
+++ Functions-variables/hello0.py
@@ -3,4 +3,4 @@
 print("Hello World!")
 
 # This run this program call the python interpreter from the terminal CLI
-# Python will run the file pass as command line argument to the compilation program.
+# Python will run the file pass as a command line argument to the compilation program.
