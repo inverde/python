2a75a2801c905d822514e0341863577f8e1e4566
inverde <mdelossantos@yahoo.com>
2024-10-16 01:22:58 +0000

    Tue, Oct 15, 2024, 9:22 PM -04:00

--- Conditionals/floor_division.py
+++ Conditionals/floor_division.py
@@ -1,3 +1,4 @@
+from conditional_execution import conditional_execution
 # Floor division executes a division and provide just the whole number as a result without fractional part.
 """ Floor division rounds down to an integer the result
 This calculation is usuful to convert to system using just whole units like days, hours, minutes and seconds """
@@ -20,3 +21,8 @@ def minutes_to_days(minutes):
 days, hours, minutes = minutes_to_days(minutes)
 
 print(f"There are {days} days, {hours} hours and {minutes} minutes in 1660 minutes")
+
+days, hours, minutes = conditional_execution(1660, True, minutes_to_days)
+
+print('Days, hours, minutes:', days, hours, minutes)
+
