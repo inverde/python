8e9ec3fae1c5520bad94304b33b8cb81290fa6f0
inverde <mdelossantos@yahoo.com>
2024-10-16 22:32:17 +0000

    Wed, Oct 16, 2024, 6:32 PM -04:00

--- Loops/repeating_messages.py
+++ Loops/repeating_messages.py
@@ -8,5 +8,11 @@ print("meaow")
 # Define a function to print any message n times instead of three
 """Our function repeat msg need to use the structure of a loop
 """
-def repeat(msg):
-    
+def repeat(msg, times):
+    # Test with1
+    # while loop
+    n = times
+    while n:
+        print(msg)
+        n-= 1
+
