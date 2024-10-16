f705728d476e1c95159efc47284219782a39f71a
inverde <mdelossantos@yahoo.com>
2024-10-16 00:12:50 +0000

    Tue, Oct 15, 2024, 8:12 PM -04:00

--- conditional_execution.py
+++ conditional_execution.py
@@ -1,1 +1,12 @@
-# Conditional execution cluase
+# Conditional execution clause
+""" It is the simplest form of the simple statement
+It has the same caractheristic of a funcion: a header and a body """
+# Sampole conditional execution
+if x > 0:
+    print('x is positive')
+
+# Abstraction passing a value, boolean expression and function
+def conditional_execution(value, test, func):
+    if test:
+        func(value)
+
