04cc9887db3e7da693bda3424af268f57a62e7d5
inverde <mdelossantos@yahoo.com>
2024-10-16 17:56:14 +0000

    Wed, Oct 16, 2024, 1:56 PM -04:00

---
+++ Conditionals/chained_conditionals.py
@@ -0,0 +1,18 @@
+# Some times we have more than two alternative branches to execute.
+""" Chained conditionals are more than two mutually exclusive execution branches
+Let's assume we need to assign letter grades to our students"""
+def grade(puntuation):
+    if puntuantion >= 90:
+        return A
+    elif puntuation >= 80:
+        return B
+    elif puntuation >= 70:
+        return C
+    elif puntuation >= 60:
+        return D
+    elif puntuation >= 0:
+        return F
+    else
+        raise VALUE_ERROR
+
+
