76f9101fdb1748137e78e3dde8ffe48200683565
inverde <mdelossantos@yahoo.com>
2024-10-16 03:30:15 +0000

    Tue, Oct 15, 2024, 11:30 PM -04:00

--- alternative_execution.py
+++ alternative_execution.py
@@ -1,6 +1,5 @@
 # Alternative execution happens when conditional statements brake into two branches
 
 # Define a function to classify number between even and odd
-def odd_even_numbers(n):
-    if n % 2 == 0:
+def altenative_execution(n, )
 
