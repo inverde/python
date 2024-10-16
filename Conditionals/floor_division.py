a97242e1ea36ffd63bd640663df4ed775ec3363b
inverde <mdelossantos@yahoo.com>
2024-10-16 00:47:50 +0000

    Tue, Oct 15, 2024, 8:47 PM -04:00

--- Conditionals/floor_division.py
+++ Conditionals/floor_division.py
@@ -1,1 +1,16 @@
-# Floor division 
+# Floor division executes a division and provide just the whole number as a result without fractional part.
+""" Floor division rounds down to an integer the result
+This calculation is usuful to convert to system using just whole units like days, hours, minutes and seconds """
+
+# Let's assume we have 1660 minutes and needs to know how many days, hours and minutes are contained
+
+minutes = 1660
+
+hours = minutes // 60
+
+if hours > 24:
+    days = hours //4
+
+remainder = minutes % 60
+
+print(f"There are {hours} hours and {remainder} minutes in 1660 minutes")
