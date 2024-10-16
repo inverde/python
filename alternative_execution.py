33e15e04b0d266651ead2e578d0276dd93593059
inverde <mdelossantos@yahoo.com>
2024-10-16 03:37:03 +0000

    Tue, Oct 15, 2024, 11:37 PM -04:00

--- alternative_execution.py
+++ alternative_execution.py
@@ -1,5 +1,13 @@
 # Alternative execution happens when conditional statements brake into two branches
 
 # Define a function to classify number between even and odd
-def altenative_execution(n, )
+def alternative_execution(n, test, main, alternate):
+    if test:
+        return main(n)
+    else:
+        return alternate(n)
+
+
+# Lets execute modulus function to print if it is even or odd
+alternative_execution(5, 5 % 2 == 0, lambda x: print(f"{x} is even"), print(f"{x} is odd"))
 
