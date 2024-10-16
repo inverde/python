9df64026227d0b8573a235747b3d09de0c1e423e
inverde <mdelossantos@yahoo.com>
2024-10-16 22:52:54 +0000

    Wed, Oct 16, 2024, 6:52 PM -04:00

--- Loops/repeating_messages.py
+++ Loops/repeating_messages.py
@@ -1,3 +1,5 @@
+# Add the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import ../Conditionals/conditionals as condMod
 # Sometimes we need to send a message several times
 """Lets write a litle program for the cat
