5c17f9749adfdd23ca45071408f8c1516abdc103
inverde <mdelossantos@yahoo.com>
2024-10-17 15:15:29 +0000

    Thu, Oct 17, 2024, 11:15 AM -04:00

--- Loops/lists.py
+++ Loops/lists.py
@@ -1,21 +1,31 @@
+# Lets include system modules to include Conditionals directory in system path
+import sys, os
+# Lets append the Conditionals directory to the path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), '../Conditionals')))
+# Import conditional module to this file
+import conditionals as condMod
+
+condMod.set_testing(condMod.c_ON)
+
 # Looping lists
+
 students = [
     'Luis Rodolfo',
     'Luis Miguel',
     'Manuel Alejandro'
 ]
 
-# Use For Loops to loop throught lists
-for student in students:
-    print(student)
+if condMod.is_testing():
+    # Use For Loops to loop throught lists
+    for student in students:
+        print(student)
 
-print()
-for i in range(len(students)):
-    print(i+1, students[i], sep=": ")
+    print()
+    for i in range(len(students)):
+        print(i+1, students[i], sep=": ")
 
-# Printing all the items individually
-print()
-print(students[0])
-print(students[1])
-print(students[2])
-
+    # Printing all the items individually
+    print()
+    print(students[0])
+    print(students[1])
+    print(students[2])
