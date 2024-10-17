4dfd20cc9e42d0a93489c08622f2f096173feed6
inverde <mdelossantos@yahoo.com>
2024-10-17 14:02:09 +0000

    Thu, Oct 17, 2024, 10:02 AM -04:00

---
+++ Loops/list_loop.py
@@ -0,0 +1,11 @@
+# Another type of loops in Python are For Loops
+
+# Lets loop through all the items of a list
+for i in [0,1,2]:
+    print("meow")
+
+# We need to loop through a iterable certain number of times
+# Lets use the Python iterable function range
+for i in range(3):
+    print("meow")
+
