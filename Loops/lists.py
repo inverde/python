e203f503ad2f3aae767af592fa2b315a9976ea9e
inverde <mdelossantos@yahoo.com>
2024-10-17 15:04:40 +0000

    Thu, Oct 17, 2024, 11:04 AM -04:00

--- Loops/lists.py
+++ Loops/lists.py
@@ -11,10 +11,10 @@ for student in students:
 
 print()
 for i in range(len(students)):
-    print(i, students[i], sep=": ")
+    print(i+1, students[i], sep=": ")
 
 # Printing all the items individually
-print(end="\n")
+print()
 print(students[0])
 print(students[1])
 print(students[2])
