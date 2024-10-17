63d0d06f2108e0a913ed2b711f05185384bb8d4f
inverde <mdelossantos@yahoo.com>
2024-10-17 14:09:20 +0000

    Thu, Oct 17, 2024, 10:09 AM -04:00

--- Loops/list_loop.py
+++ Loops/list_loop.py
@@ -1,5 +1,14 @@
-# Another type of loops in Python are For Loops
+# import sys and os module to add other directory than current to the path
+import sys, os
+os.path.join(os.path.directory(__name__), '../Conditionals')
+import Conditionals as condMod
 
+
+# Another type of loops in Python are For Loops
+""" For loops loops iterate all the values of iterable sequence of values
+    In Python a iterable is a class of object able to makes iterations
+    There are several structures like: lists, tuples, dictionaries and collections
+"""
 # Lets loop through all the items of a list
 for i in [0,1,2]:
     print("meow")
