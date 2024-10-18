35ecd361ac5887dd143782ef43b2d9ae50fe0171
inverde <mdelossantos@yahoo.com>
2024-10-18 03:15:46 +0000

    Thu, Oct 17, 2024, 11:15 PM -04:00

--- Loops/directory_loop.py
+++ Loops/directory_loop.py
@@ -1,21 +1,22 @@
-students = {electoral_id: '00100973486',
-            personal_data: {
-                firstName: 'Manuel',
-                middleName: 'Oscar',
-                lastName: 'De Los Santos'
-                Address: 'Calle Santa Teresa #10, Torre DKI, Apt 7A',
-                telephone: 8099095551,
-                dayOfBirth: '11-06-1961',
-                civilStatus: 'Single'
+students = {
+            'electoral_id': '00100973486',
+            'personal_data': {
+                'firstName': 'Manuel',
+                'middleName': 'Oscar',
+                'lastName': 'De Los Santos',
+                'Address': 'Calle Santa Teresa #10, Torre DKI, Apt 7A',
+                'telephone': 8099095551,
+                'dayOfBirth': '11-06-1961',
+                'civilStatus': 'Single'
             },
-            academic_record: {
-                major: 'Computer Sciences',
-                studentID: 180584,
-                GPA: 3.60,
-                coursesTaken: ['Python', 'C Language', 'Algorithms', 'Web Development'],
-                academicLoad: ['Data Science', 'Pre-Algebra']
+            'academic_record': {
+                'major': 'Computer Sciences',
+                'studentID': 180584,
+                'GPA': 3.60,
+                'coursesTaken': ['Python', 'C Language', 'Algorithms', 'Web Development'],
+                'academicLoad': ['Data Science', 'Pre-Algebra']
             }
 }
 
-for key, value in students.keys, students.values:
-    print(key, value)
+for student in students:
+    print(student, student)
