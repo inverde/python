c3e26295adf144e2caf3b7980a5c00a47e25d432
inverde <mdelossantos@yahoo.com>
2024-10-18 19:13:00 +0000

    Fri, Oct 18, 2024, 3:13 PM -04:00

--- Loops/directory_loop.py
+++ Loops/directory_loop.py
@@ -30,6 +30,6 @@ for classmate in phoneBook:
     print(f"El teléfono de {classmate} es {formatted_string}")
 
 for student in students:
-    f"Estudiante {student['personal_data']['lastName']}, matrícula {student['academic_record']['studentID']}
+    f"Estudiante {student['personal_data']['lastName']}, matrícula {student['academic_record']['studentID'] es de {student['academic_record']['major']}}
     print(student['academic_record']['studentID'], student['personal_data']['lastName'],
           student['academic_record']['major'])
