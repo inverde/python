de0a351a1dc9e91a6f1a78375a5d1aff2309048e
inverde <mdelossantos@yahoo.com>
2024-10-18 02:48:37 +0000

    Thu, Oct 17, 2024, 10:48 PM -04:00

---
+++ Loops/directory_loop.py
@@ -0,0 +1,13 @@
+students = {telephone: 8099095551,
+            personalData: {
+                firstName: 'Manuel',
+                middleName: 'Oscar',
+                lastName: 'De Los Santos'
+                Address: 'Calle Santa Teresa #10, Torre DKI, Apt 7A',
+                electoralID: 00100973486,
+                dayOfBirth: '11-06-1961',
+                civilStatus: 'Single'
+            },
+            major: 'Economics'
+            studentID: 795584
+}
