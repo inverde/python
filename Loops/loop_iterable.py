e0c2375638748613494f80046fd389d74c412ce9
inverde <mdelossantos@yahoo.com>
2024-10-19 03:06:12 +0000

    Fri, Oct 18, 2024, 11:06 PM -04:00

--- Loops/loop_iterable.py
+++ Loops/loop_iterable.py
@@ -43,7 +43,7 @@ class shoppingCartIterator:
 def shoppingCart_factory(n):
     return shoppingCart(0, n)
 
-condMod.set_testing(condMod.c_OFF)
+# condMod.set_testing(condMod.c_OFF)
 
 if __name__ == "__main__":
     if condMod.is_testing():
