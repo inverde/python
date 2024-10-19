a5b9d642122b04537a2574bc7caaf602314c13ad
inverde <mdelossantos@yahoo.com>
2024-10-19 14:52:06 +0000

    Sat, Oct 19, 2024, 10:52 AM -04:00

--- Exceptions/retry.py
+++ Exceptions/retry.py
@@ -1,3 +1,10 @@
 # When user fails input ask again
 while True:
+    try:
+        n = int(input("Please enter a number:? "))
+
+    except ValueError:
+        print("Value must be a integer")
+    else:
+        break
     
