51619b70b5acacaa316dc393346cd623fd971bb8
inverde <mdelossantos@yahoo.com>
2024-10-19 14:27:24 +0000

    Sat, Oct 19, 2024, 10:27 AM -04:00

--- Exceptions/hello.py
+++ Exceptions/hello.py
@@ -1,15 +1,4 @@
 # Print hello with syntax error
 print("Helo World!!")
 
-print()
-
-# Shows an example of runtime error
-
-try:
-
-    n = int(input("Please enter number of times? "))
-
-    Exception:
-
-        print("Value must be a integer!!!")
 
---
+++ Exceptions/number.py
@@ -0,0 +1,9 @@
+# Shows an example of runtime error
+
+try:
+
+    n = int(input("Please enter number of times? "))
+
+    Exception:
+
+        print("Value must be a integer!!!")
