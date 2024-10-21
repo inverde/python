92c9838dbabb2b7b7633e783f8f14a6afb23a248
inverde <mdelossantos@yahoo.com>
2024-10-21 16:06:02 +0000

    Mon, Oct 21, 2024, 12:06 PM -04:00

--- Libraries/librdom.py
+++ Libraries/librdom.py
@@ -2,5 +2,8 @@
 # Lets import the random library
 import random
 # Generate a list random number
-numbers = random.randint(1, 10)
+for i in range(1, 10)
+    number = random.randint(1, 10)
+    numbers.append(number)
 print(numbers)
+
