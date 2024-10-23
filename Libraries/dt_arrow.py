81d8edb99c7adea65fffcf503bcc023444b50e6a
inverde <mdelossantos@yahoo.com>
2024-10-23 04:50:22 +0000

    Wed, Oct 23, 2024, 12:50 AM -04:00

--- Libraries/dt_arrow.py
+++ Libraries/dt_arrow.py
@@ -8,6 +8,7 @@ years = delta.days//365
 days = delta.days % 365
 if days > 31:
     months = days//30
+    days = days % 30
 print(years, days)
 year_days = 31+28+31+30+31+30+31+31+30+31+30+31
 print(year_days)
