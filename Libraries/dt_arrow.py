d2ca5cebd644c55c085930cb3e85ee2ca3f476ab
inverde <mdelossantos@yahoo.com>
2024-10-23 04:40:10 +0000

    Wed, Oct 23, 2024, 12:40 AM -04:00

--- Libraries/dt_arrow.py
+++ Libraries/dt_arrow.py
@@ -3,5 +3,7 @@ birth = arrow.get(1961, 6, 11)
 print(birth.humanize())
 today = arrow.utcnow()
 delta = today - birth
-print(delta.years)
-
+print(delta.days)
+years = delta.days//365
+days = delta.days % 365
+print(years, days)
