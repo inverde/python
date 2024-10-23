369131bf59ebb94e9ee19dafcd6b42cebd010c98
inverde <mdelossantos@yahoo.com>
2024-10-23 05:07:26 +0000

    Wed, Oct 23, 2024, 1:07 AM -04:00

--- Libraries/dt_arrow.py
+++ Libraries/dt_arrow.py
@@ -2,14 +2,14 @@ import arrow
 birth = arrow.get(1961, 6, 11)
 print(birth.humanize())
 today = arrow.now("US/Eastern")
-print(today)
+print(today.month)
 delta = today - birth
 print(delta.days)
 years = delta.days//365
 days = delta.days % 365
 if days > 31:
-    months = days//31
-    days = days % 31
+    months = today.month - birth.month
+    days = days - (months * 30)
     print(years, months, days)
 else:
     print(years, days)
