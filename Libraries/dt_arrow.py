cb3bddce3ea034179fa2bd674f7ab308f583b97e
inverde <mdelossantos@yahoo.com>
2024-10-23 05:36:46 +0000

    Wed, Oct 23, 2024, 1:36 AM -04:00

--- Libraries/dt_arrow.py
+++ Libraries/dt_arrow.py
@@ -13,7 +13,7 @@ if days > 31:
     leap_days = 63//4
     year_days = years * 365
     month_days = delta - year_days - regular_days - leap_days
-    
+    print(year_days, month_days, regular_days, leap_days)
     print(years, months, regular_days, leap_days)
 else:
     print(years, days)
