ea4afd297a36390288246f620b33626db39363fa
inverde <mdelossantos@yahoo.com>
2024-10-23 04:49:30 +0000

    Wed, Oct 23, 2024, 12:49 AM -04:00

--- Libraries/dt_arrow.py
+++ Libraries/dt_arrow.py
@@ -6,6 +6,8 @@ delta = today - birth
 print(delta.days)
 years = delta.days//365
 days = delta.days % 365
+if days > 31:
+    months = days//30
 print(years, days)
 year_days = 31+28+31+30+31+30+31+31+30+31+30+31
 print(year_days)
