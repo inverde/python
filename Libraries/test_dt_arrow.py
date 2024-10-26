b1192ce0980877e0a1fdb1660a7ed534c023ea12
inverde <mdelossantos@yahoo.com>
2024-10-26 02:13:44 +0000

    Fri, Oct 25, 2024, 10:13 PM -04:00

--- Libraries/test_dt_arrow.py
+++ Libraries/test_dt_arrow.py
@@ -5,7 +5,7 @@ def test_days_after_birth():
     assert days_after_birth(2024, 10,20) == (6, 0)
 
 def test_years_from_birth():
-    assert years_from_birth(5, 0) == (0,0)
+    assert years_from_birth(6, 0) == (0,6)
 
 def test_leap_years_in_range():
     pass
