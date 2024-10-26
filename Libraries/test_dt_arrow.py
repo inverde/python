4502b2f075e3c72657b766666cc62f99a5bf19c3
inverde <mdelossantos@yahoo.com>
2024-10-26 01:49:30 +0000

    Fri, Oct 25, 2024, 9:49 PM -04:00

--- Libraries/test_dt_arrow.py
+++ Libraries/test_dt_arrow.py
@@ -1,3 +1,14 @@
 import pytest
 from dt_arrow import days_after_birth, years_from_birth, leap_years_in_range
 
+
+def test_days_after_birth():
+    pass
+
+def test_years_from_birth():
+    pass
+
+def test_leap_years_in_range():
+    pass
+
+
