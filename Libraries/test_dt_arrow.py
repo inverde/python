42f53e0945d51694dc6990db358538b21cec555a
inverde <mdelossantos@yahoo.com>
2024-10-26 02:20:37 +0000

    Fri, Oct 25, 2024, 10:20 PM -04:00

--- Libraries/test_dt_arrow.py
+++ Libraries/test_dt_arrow.py
@@ -9,6 +9,6 @@ def test_years_from_birth():
     assert years_from_birth(5, 0) == (0,5)
 
 def test_leap_years_in_range():
-    assert leap_years_in_range(arrow.get(2024,01,01), arrow.now('US/Eastern')) == 1
+    assert leap_years_in_range((2020, 2024)) == 1
 
 
