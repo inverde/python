caa50390776f69b99cf679ca297a1b1e04b3c2b2
inverde <mdelossantos@yahoo.com>
2024-10-27 22:50:14 +0000

    Sun, Oct 27, 2024, 6:50 PM -04:00

--- Filesio/names.py
+++ Filesio/names.py
@@ -7,7 +7,11 @@ for _ in range(3):
     names.append(name)
     file.close()
 
+for name in name:
+    print(name)
+
     """Program weakness
         1. Do not catch error for user input
         2. Every time writes to file must open it and close it
+        3. Do not distinguish one input from another in file. Not delimiter in file
     """
