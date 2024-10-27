3fbb20bcbde73b600b81d0a55bd84a5881c25cd1
inverde <mdelossantos@yahoo.com>
2024-10-27 22:01:15 +0000

    Sun, Oct 27, 2024, 6:01 PM -04:00

--- Filesio/names_append.py
+++ Filesio/names_append.py
@@ -1,6 +1,19 @@
+names = []
+
+def get_name():
+    while True:
+        try:
+            return input("What's your name:? ")
+        except ValueError:
+            continue
+
 for _ in range(3):
     name = get_name()
     file = open("names.txt", 'a')
     file.write(name)
+    names.append(name)
+    file.close()
 
-file.close()
+for name in names:
+    print(name)
+
