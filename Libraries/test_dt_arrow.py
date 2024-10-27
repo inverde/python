4e3ddc77440a0ade8eac4c754cf086e1ebe8b705
inverde <mdelossantos@yahoo.com>
2024-10-27 21:31:27 +0000

    Sun, Oct 27, 2024, 5:31 PM -04:00

--- Libraries/test_dt_arrow.py
+++ Libraries/test_dt_arrow.py
@@ -5,14 +5,15 @@ from dt_arrow import days_after_birth, years_from_birth, leap_years_in_range
 
 warnings.filterwarnings("ignore")
 
-def test_days_after_birth():
-    with pytest.raises(AssertionError):
-        assert days_after_birth(2024, 10,20) == (6, 0)
+with warnings.catch_warnings():
+    def test_days_after_birth():
+        with pytest.raises(AssertionError):
+            assert days_after_birth(2024, 10,20) == (6, 0)
 
-def test_years_from_birth():
-    assert years_from_birth(5, 0) == (0,5)
+    def test_years_from_birth():
+        assert years_from_birth(5, 0) == (0,5)
 
-def test_leap_years_in_range():
-    with pytest.raises(AssertionError):
-        assert leap_years_in_range((2020, 2024)) == 0
+    def test_leap_years_in_range():
+        with pytest.raises(AssertionError):
+            assert leap_years_in_range((2020, 2024)) == 0
 
