f98d3f768ecc25d7537b5452bf128e9a0e2f4e24
inverde <mdelossantos@yahoo.com>
2024-10-28 15:30:03 +0000

    Mon, Oct 28, 2024, 11:30 AM -04:00

--- Filesio/names_read3.py
+++ Filesio/names_read3.py
@@ -1,7 +1,7 @@
 # Shows how to read a file with CSV Package
 import csv
 with open("names.csv") as file:
-    reader = csv.(file)
+    reader = csv.reader(file)
     for line in reader:
         name, city = line.rstrip().split(',')
         print(f"{name} lives in {city}")
