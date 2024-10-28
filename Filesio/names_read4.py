8b7c7b65c01e87f903407dca76ad9fac8c14ad68
inverde <mdelossantos@yahoo.com>
2024-10-28 15:42:23 +0000

    Mon, Oct 28, 2024, 11:42 AM -04:00

--- Filesio/names_read4.py
+++ Filesio/names_read4.py
@@ -1,6 +1,6 @@
 import csv
 # Unpacking list with csv package
-with open("names.csv") as file:
+with open("names.csv", 'r') as file:
     reader = csv.reader(file)
     for line in reader:
         name, city = line
