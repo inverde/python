32dc76e53cb25b552ef6afc7325b9d4e6e955bc1
inverde <mdelossantos@yahoo.com>
2024-10-28 15:31:42 +0000

    Mon, Oct 28, 2024, 11:31 AM -04:00

--- Filesio/names_read3.py
+++ Filesio/names_read3.py
@@ -3,5 +3,4 @@ import csv
 with open("names.csv") as file:
     reader = csv.reader(file)
     for line in reader:
-        name, city = line.rstrip().split(',')
         print(f"{name} lives in {city}")
