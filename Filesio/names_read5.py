2c1cea8056627a06a8e300b809ab960235125f44
inverde <mdelossantos@yahoo.com>
2024-10-28 15:48:00 +0000

    Mon, Oct 28, 2024, 11:48 AM -04:00

---
+++ Filesio/names_read5.py
@@ -0,0 +1,11 @@
+import csv
+# Unpacking list with csv package
+# We need to sort all messages before printing in this version
+names = []
+with open("names.csv", 'r') as file:
+    reader = csv.reader(file)
+    for line in reader:
+        name, city = line
+        names.append(f"{name} lives in {city}")
+    for name in names:
+        print(f"{name} lives in {city}")
