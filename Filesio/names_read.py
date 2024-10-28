e091844701a9d9a57ae1a5c9efaae817a71bac4a
inverde <mdelossantos@yahoo.com>
2024-10-28 14:42:41 +0000

    Mon, Oct 28, 2024, 10:42 AM -04:00

--- Filesio/names_read.py
+++ Filesio/names_read.py
@@ -1,5 +1,6 @@
 with open("names.csv") as file:
     lines = file.readlines()
     for line in lines:
+        line = line.rstrip().split(',')
         print(f"{line[0]} lives in {line[1]}")
-        
+
