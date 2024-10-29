358c89dce6598163f62d677dda0f5a38e977e7e2
inverde <mdelossantos@yahoo.com>
2024-10-29 02:20:46 +0000

    Mon, Oct 28, 2024, 10:20 PM -04:00

--- Filesio/names_writer1.py
+++ Filesio/names_writer1.py
@@ -1,5 +1,9 @@
 # We need to write a dictionary to the file
 import csv
+
+name = input("What's your name, please:? ")
+city = input("Pleae enter city your live in:? ")
+
 with open("residents.csv", 'a') as file:
-    writer = csv.Dictwriter(file, fieldnames=["name", "city"])
-    writer.writerow()
+    writer = csv.DictWriter(file, fieldnames=["name", "city"])
+    writer.writerow({"name": name, "city": city})
