aa9044b17d6063544be00c99a9b9dd5f7f78b7a1
inverde <mdelossantos@yahoo.com>
2024-10-29 02:08:31 +0000

    Mon, Oct 28, 2024, 10:08 PM -04:00

---
+++ Filesio/names_writer.py
@@ -0,0 +1,8 @@
+name = input("What's your name:? ")
+city = input("Please enter name of your city:? ")
+
+# Open a csv file for writer purposes using the csv package
+with open("names.csv", 'a') as file:
+    writer = csv.writer(file)
+    writer.writerow([name, city])
+
--- names_writer.py
+++
@@ -1,4 +0,0 @@
-# Open a csv file for writer purposes using the csv package
-with open("names.csv", 'a') as file:
-    writer = csv.writer(file)
-    
