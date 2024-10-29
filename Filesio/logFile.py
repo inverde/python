d59667bad3951636c9c717ffe2e22946a3189fbc
inverde <mdelossantos@yahoo.com>
2024-10-29 03:26:39 +0000

    Mon, Oct 28, 2024, 11:26 PM -04:00

--- Filesio/logFile.py
+++ Filesio/logFile.py
@@ -1,7 +1,7 @@
 # In this program will be logging messages to a debug file
 
 # Please write a decorator function to help trace my code when testing
-def traceCallParams(func):
+def traceCall2(func):
     def wrapper(arg1, arg2):
         print(f"Before calling {func.__name__}")
         resultado = func(arg1, arg2)
@@ -9,10 +9,18 @@ def traceCallParams(func):
         return resultado
     return wrapper
 
-@traceCall
+def traceCall1(func):
+    def wrapper(arg1):
+        print(f"Before calling {func.__name__}")
+        func(arg1)
+        print(f"After calling {func.__name__}")
+    return wrapper
+
+@traceCall2
 def openFile(filename, mode):
     return open(filename, mode)
 
+@traceCall1
 def closeFile(file):
     file.close()
 
