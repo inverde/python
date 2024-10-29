ac639035b51f558222970fc1c3ea3c79efdfc100
inverde <mdelossantos@yahoo.com>
2024-10-29 03:04:10 +0000

    Mon, Oct 28, 2024, 11:04 PM -04:00

--- Filesio/logFile.py
+++ Filesio/logFile.py
@@ -8,11 +8,11 @@ def traceCall(func):
         print("After calling {func.__name__}")
     return wrapper
 
-@traceCall
+@traceCall()
 def openFile(filename, mode):
     return open(filename, mode)
 
-@traceCall
+@traceCall()
 def closeFile(file):
     file.close()
 
