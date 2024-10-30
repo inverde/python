7904f3689cbb18c6e4426d59cdde54d72df8f770
inverde <mdelossantos@yahoo.com>
2024-10-30 22:09:53 +0000

    Wed, Oct 30, 2024, 6:09 PM -04:00

---
+++ Regexs/valid_email5.py
@@ -0,0 +1,28 @@
+import re
+""" Regular Expressions:
+    .   any character except a new line
+    *   0 or more repetitions
+    +   1 or more repetitions
+    ?   0 or 1 repetition
+    {m} m repetitions
+    {m,n} m-n repetitions
+"""
+def validate_email():
+    """Simple function to see if user input is a email
+    no arguments() none
+
+    return boolean: True or False
+    """
+
+    email = input("What's your email:? ").strip()
+
+
+    if re.search(r"^(.+)@(.+)\.(edu)$",email, re.IGNORECASE):
+        return True
+    else:
+        return False
+
+if validate_email():
+    print("Valid email")
+else:
+    print("Invalid email")
