3279fa9d3c7f8809e36eb59d05d53191ad044d2f
inverde <mdelossantos@yahoo.com>
2024-10-30 03:59:57 +0000

    Tue, Oct 29, 2024, 11:59 PM -04:00

--- Regexs/valid_email1.py
+++ Regexs/valid_email1.py
@@ -13,9 +13,13 @@ def validate_email():
         return False
 """ How many other rules we need to add for a valid a email
     Add at least three of them:
-    1.
-    2.
-    3.
+    1. Don't allow less than 8 characters in your username
+    2. Allow a period, underscore or hiphen in your username but not all
+    3. The username shouldn't start with any special characters or a number
+    4. We shouldn't allow more than one @ in the email
+    5. The server name may include governmental, commercial, educational and non profit only
+    6. The server name may until three compound domain name, or up to 2 dots in the name
+
 """
 if validate_email():
     print("Valid")
