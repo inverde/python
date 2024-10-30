b5f4a3275774c2911f6a42d0e738f293a62de403
inverde <mdelossantos@yahoo.com>
2024-10-30 21:43:31 +0000

    Wed, Oct 30, 2024, 5:43 PM -04:00

---
+++ Regexs/valid_email4.py
@@ -0,0 +1,30 @@
+# Using the built-in library re make sure we use regular expressions to validate email
+import re
+""" Regular Expressions:
+    .   any character except a new line
+    *   0 or more repetitions
+    +   1 or more repetitions
+    ?   0 or 1 repetition
+    {m} m repetitions
+    {m,n} m-n repetitions
+"""
+def validate_email():
+    """Simple function to see if user input is a email
+    no arguments() none
+
+    return boolean: True or False
+    """
+
+    email = input("What's your email:? ").strip()
+
+    username, domain = email.split('@')
+
+    if re.search)(r"(\w+)@(\w+)\.edu)
+        return True
+    else:
+        return False
+
+if validate_email():
+    print("Valid email")
+else:
+    print("Invalid email")
