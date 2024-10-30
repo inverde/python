13fbfda9f7a18c4a9bab1a9df7a98f217c68d2db
inverde <mdelossantos@yahoo.com>
2024-10-30 05:38:00 +0000

    Wed, Oct 30, 2024, 1:38 AM -04:00

--- Regexs/valid_email2.py
+++ Regexs/valid_email2.py
@@ -1,23 +1,7 @@
 # Validating email with regex
 import re
 def validate_email():
-    """ Function to validate email with regular expressions following these rules:
-    1. Don't allow less than 8 characters in your username
-    2. Allow a period, underscore or hiphen in your username but not all
-    3. The username shouldn't start with any special characters or a number
-    4. We shouldn't allow more than one @ in the email
-    5. The server name may include governmental, commercial, educational and non profit only
-    6. The server name may until three compound domain name, or up to 2 dots in the name
 
-    no arguments() none
-
-    return boolean: True or False
-
-    Validating two groups of characters: username and servername
-    username:[\w][\.|-|_]?{8,20}
-    servername: [^@][\w][.com|.edu|.gov|.gob|.org]$
-
-    """
     formatUser = r"(\w)(\.|-|_)?{8,20}"
 
     formatDomain = r"^(^@\w)\.(com|edu|gov|gob|org]$"
