06fe13865937c1c2ab56eb518a91ed186e4fa2cb
inverde <mdelossantos@yahoo.com>
2026-02-05 14:51:28 +0000

    Thu, Feb 5, 2026, 10:51 AM -04:00

--- static/py/menu_links.py
+++ static/py/menu_links.py
@@ -7,7 +7,7 @@ menu_linktype = {"en":{"label":str, "url":str},
 school_menu = menu_linktype
 
 def menu_entry(menu:dir = school_menu, url:str, label_en, label_es:str = 'None', url_link:str)-> None:
-    menu["en"]["label"] = label_en
+    menu["en"][label_en] = label_en
     menu["es"]["url"] = url_link
 
 
