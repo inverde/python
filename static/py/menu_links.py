c8df21112e1540d5ec3fb3fe5cc537b5a0e8332a
inverde <mdelossantos@yahoo.com>
2026-02-05 14:38:48 +0000

    Thu, Feb 5, 2026, 10:38 AM -04:00

--- static/py/menu_links.py
+++ static/py/menu_links.py
@@ -1,9 +1,13 @@
 # Declare globals types
 
 menu_linktype = {"en":{"url":str, "label":str},
-                 "es":{"url:str, "label":str}
+                 "es":{"url":str, "label":str}
 }
 
 school_menu = menu_linktype
 
+def menu_entry(menu:dir, label:str, url:str)-> None:
+    global lang
+    if lang=="es":
+        
 
