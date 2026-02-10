7d678abf9dc46a2e21ebaa8e0059036a4d778de5
inverde <mdelossantos@yahoo.com>
2026-02-10 00:34:17 +0000

    Mon, Feb 9, 2026, 8:34 PM -04:00

--- static/py/menu_links.py
+++ static/py/menu_links.py
@@ -1,16 +1,24 @@
 # Declare globals types
 
 menu_type = {
-    [
-        {"label_en", str, "label_es":str, "url":str}
-    ]
+                "school":[
+                    {"label_en":str, "label_es":str, "url":str}
+                ],
+                "admin":[
+                    {"label_en":str, "label_es":str, "url":str}
+                ]
 }
+
+lang = "es"
+
 
 school_menu = menu_type
 admin_menu = menu_type
+courses_menu = menu_type
 
-def menu_entry(menu:dir = school_menu, label_en, label_es:str = 'None', url_link:str)-> None:
-    menu["en"].append({"label":label_en, "url": url_link})
-    menu["es"].append({"label": label_es, "url": url_link})
+
+def menu_entry(label_en:str,url_link:str, label_es:str = 'None', menu:dir = courses_menu)-> None:
+    menu.append({"label_en":label_en, "label_es":label_es, "url": url_link})
+
 
 
