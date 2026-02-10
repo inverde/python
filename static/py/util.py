195b42bad88da82f8ce9888cc083c831b1824905
inverde <mdelossantos@yahoo.com>
2026-02-10 22:13:19 +0000

    Tue, Feb 10, 2026, 6:13 PM -04:00

--- static/py/util.py
+++ static/py/util.py
@@ -114,14 +114,5 @@ if __name__=='__main__':
 
 if __name__ == "__main__":
     #Example usage: process a single file for demonstration
-
-
-
-
-
-
-
-
-
-
-
+    p = Path("example.py")
+    process_file(p, action="comment", dry_run=True, )
