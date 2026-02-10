f2cb93b2b2f4167a89d8c5f5fea1d31ba7d15676
inverde <mdelossantos@yahoo.com>
2026-02-10 02:38:26 +0000

    Mon, Feb 9, 2026, 10:38 PM -04:00

--- static/py/util.py
+++ static/py/util.py
@@ -26,6 +26,15 @@ END_MARKER = r"// INSTRUMENTATION END"
 # For Python files we willl use '#' to commnet; for other languages you can adapt
 COMMENT_PREFIX = "# "
 
+def process_text(text:str, action:str, comment_prefix:str) -> str:
+    """
+    Replace blocks between START_MARKER and END_MARKER.
+    - action == "comment": replace block contents with commented lines
+    - action == "uncomment": remove leading comment_prefix from lines inside block
+    """
+    pattern = re.compile(
+        rf"({re.escape(START_MARKER)}\n)(.*?)
+    )
 
 
 def slug(s)->str:
