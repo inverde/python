99815c39d720258e3187978a24c1a7a4419cc940
inverde <mdelossantos@yahoo.com>
2026-02-14 19:59:27 +0000

    Sat, Feb 14, 2026, 3:59 PM -04:00

--- static/py/loadCourse.py
+++ static/py/loadCourse.py
@@ -94,6 +94,7 @@ def course_data_from_json(path_dir, path_file)-> dict:
         chapter_dict["ChapterTitle_en"] = chapter["ChapterTitle_en"]
         chapter_dict["ChapterTitle_es"] = chapter["ChapterTitle_es"]
         chapter_dict["chapterURL"] = chapter["url"]
+        chapter_dict["ChapterSummary_en"] = chapter["ChapterSummary_en"]
 
 
     course_dict = {"CourseTitle": data["CourseTitle"], "Chapters": chapters}
