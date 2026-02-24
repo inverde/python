5200e18722479a1eafb68a870fdc44e3109c2702
inverde <mdelossantos@yahoo.com>
2026-02-24 23:45:43 +0000

    Tue, Feb 24, 2026, 7:45 PM -04:00

--- Flask-projects/survey/models/__init__.py
+++ Flask-projects/survey/models/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-std_base
+std_base = None
 
 def get_Base():
     global std_base
