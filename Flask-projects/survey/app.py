73cdf659bcebe14aded83b05976e18b1269bf252
inverde <mdelossantos@yahoo.com>
2026-02-24 23:33:36 +0000

    Tue, Feb 24, 2026, 7:33 PM -04:00

--- Flask-projects/survey/app.py
+++ Flask-projects/survey/app.py
@@ -1,6 +1,9 @@
 from sqlalchemy import inspect
 from models.__init__ import Base, engine
+import logging
+import sys, os
 
+"""
 # Import all models so they are registered with base
 from models.region import Region
 from models.district import District
@@ -10,6 +13,14 @@ from models.respondent import Respondent
 from models.section import Section
 from models.question import Question
 from models.answer import Answer
+"""
+#original_stdout = sys.stdout
+#sys.stdout = open(os.devnull, 'w')  # Redirect stdout to null to suppress output
+#sys.stdout = sys.__stdout__
+
+logging.getLogger("sqlalchemy").setLevel(logging.WARNING)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+logging.getLogger("sqlalchemy.dialects").setLevel(logging.WARNING)
 
 
 tablenames = [
@@ -23,21 +34,35 @@ tablenames = [
     'Answers'
 ]
 
-# Check if a specific table is missing
-
-inspector = inspect(engine)
-
-print(inspector.get_table_names())
-
-for tablename in tablenames:
-    if tablename in inspector.get_table_names():
-        print(f"Table '{tablename}' already exists.")
+def init_db():
+    """
+    Initializes the database by creating all tables defined in the models.
+    Check if any tables from `tablenames` list are missing before creating them.
+    If any tables are missing, it will print a message indicating which tables need to be created.
+    """
+    # Use global variable to access the list of expected table names
+    global tablenames
+    # Get all the existing table names from the database
+    expected_tables = set(tablenames)
+    db_tables = set(inspect(engine).get_table_names())
+    missing = set([ table for table in tablenames if  table not in db_tables])
+    if expected_tables.issubset(db_tables):
+        # All tables are present, no need to create them
+        print("All tables have been created already !!!")
+    elif missing == expected_tables:
+        print("Database is empty, creating all tables...")
+        if engine.url.database:
+            db_path = os.path.abspath(engine.url.database)
+            print(f"Database file will be created at path: {db_path}")
+            print("Priting all tables imported into Base metadata:", Base.metadata.tables.keys())
+        #Base.metadata.create_all(engine)
+        #print("Base de datos creada exitosamente.")
+        #print(missing)
     else:
-        print("Please create all missing tables in engine")
-        break
+        print("Some tables are missing:", missing)
 
 
-# Crear todas las tablas
-#Base.metadata.create_all(engine)
-
+#sys.stdout = original_stdout
 #print("Base de datos creada exitosamente.")
+if __name__ == "__main__":
+    init_db()
--- Flask-projects/survey/models/__init__.py
+++ Flask-projects/survey/models/__init__.py
@@ -2,12 +2,31 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+std_base
+
+def get_Base():
+    global std_base
+    if std_base is None:
+        std_base = declarative_base()
+    return std_base
+
+Base = get_Base()
+
+engine = create_engine("sqlite:////workspaces/python/Flask-projects/survey/data/instance/app.sqlite3.db", echo=True)
+
+from models.region import Region
+from models.district import District
+from models.building import Building
+from models.school import School
+from models.respondent import Respondent
+from models.section import Section
+from models.question import Question
+from models.answer import Answer
+
 
 # SQLITE Engine
 
 
-engine = create_engine("sqlite:////workspaces/python/Flask-projects/survey/data/instance/app.sqlite3", echo=True)
 
 # Probar conexión inmediatamente
 try:
