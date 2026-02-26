dd10331fd3a8c7088c8840f33d90d55690378cc7
inverde <mdelossantos@yahoo.com>
2026-02-26 18:14:42 +0000

    Thu, Feb 26, 2026, 2:14 PM -04:00

--- Flask-projects/survey/repositories/__init__.py
+++ Flask-projects/survey/repositories/__init__.py
@@ -1,9 +1,3 @@
 # repositories/__init__.py
 # # Makes 'repositories' a package
-from .survey_repo import (
-    create_survey,
-    get_survey,
-    get_all_surveys,
-    update_survey,
-    delete_survey
-)
+from survey.repositories import survey_repo
