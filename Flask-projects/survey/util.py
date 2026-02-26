0a32c8efe58d24afb233764d0df2c3157367b19d
inverde <mdelossantos@yahoo.com>
2026-02-26 14:29:14 +0000

    Thu, Feb 26, 2026, 10:29 AM -04:00

--- Flask-projects/survey/util.py
+++ Flask-projects/survey/util.py
@@ -2,6 +2,7 @@ import app
 from sqlalchemy import create_engine, Metadata
 
 # Create engine
+engine = create_engine()
 
 tables=['Regions', 'Districts', 'Buildings', 'Schools', 'Respondents', 'Sections', 'Questions', 'Answers']
 
