eb70ed84bab8fd634beefa25f5540f4049d9e427
inverde <mdelossantos@yahoo.com>
2026-02-26 15:28:38 +0000

    Thu, Feb 26, 2026, 11:28 AM -04:00

--- Flask-projects/survey/models/answer.py
+++ Flask-projects/survey/models/answer.py
@@ -11,7 +11,7 @@ class Answer(Base):
     __tablename__ = "Answers"
 
     ID = Column(Integer, primary_key=True, autoincrement=True)
-    RespondentID = Column(Integer, ForeignKey("Respondents.RespondentID"), nullable=False)
+    RespondentID = Column(Integer, ForeignKey("Respondents.ID"), nullable=False)
     QuestionID = Column(Integer, ForeignKey("Questions.ID"), nullable=False)
     AnswerValue = Column(Text, nullable=False)  # store numeric as text or JSON; convert as needed
     AnswerTimestamp = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
@@ -20,7 +20,7 @@ class Answer(Base):
     question = relationship("Question", back_populates="answers")
 
     def __init__(self, RespondentID, QuestionID, AnswerValue, AnswerTimestamp=None):
-        self.RespondentID = RespondentID
+        self.D = RespondentID
         self.QuestionID = QuestionID
         self.AnswerValue = AnswerValue
         self.AnswerTimestamp = AnswerTimestamp or datetime.datetime.utcnow()
