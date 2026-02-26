f4dc057e56c612d68a808af8838345081523594f
inverde <mdelossantos@yahoo.com>
2026-02-26 14:38:42 +0000

    Thu, Feb 26, 2026, 10:38 AM -04:00

--- Flask-projects/survey/util.py
+++ Flask-projects/survey/util.py
@@ -1,9 +1,23 @@
-import app
 from sqlalchemy import create_engine, Metadata
+# Get the path from a SQLite database file
 
-# Create engine
-engine = create_engine()
+db_path='/worspaces/python/Flask-projects/survey/data/instance/app.sqlite3.db'
 
+
+def drop_all_tables(db_path:str):
+    """
+    Drops all tables from a SQLite database file.
+
+    Parameters:
+        db_path(str): Path to the SQLite databse file, e.g. 'example.db'
+    """
+    # Create engine
+    engine = create_engine(f"sqlite:///{db_paty}", echo=True)
+
+    # Reflect existing database schema
+    metadata = Metadata()
+    metadata.reflect(bind=engine)
+    
 tables=['Regions', 'Districts', 'Buildings', 'Schools', 'Respondents', 'Sections', 'Questions', 'Answers']
 
 if __name__ == "__main__":
