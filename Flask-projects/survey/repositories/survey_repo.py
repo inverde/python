6b8d4d986e017e2048a387564d5cf06607ed5aa1
inverde <mdelossantos@yahoo.com>
2026-02-27 16:13:21 +0000

    Fri, Feb 27, 2026, 12:13 PM -04:00

--- Flask-projects/survey/repositories/survey_repo.py
+++ Flask-projects/survey/repositories/survey_repo.py
@@ -1,3 +1,14 @@
 from sqlalchemy.orm import Session
 from survey.models.survey import Survey
 
+# CRUD functions for survey insert
+def create_survey(session:Session,
+                  SurveyTitle,
+                  SurveyPurpose=None,
+                  Year=None,
+                  Release=None,
+                  SponsorInstitution=None):
+    new_survey = Survey(
+        SurveyTitle=SurveyTitle,
+        SurveyPurpose=SurveyPurpose,
+    )
